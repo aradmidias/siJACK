ed6d6560b80a3359cd288b432e717cd23cfd3564
Hans Joachim Multhaupt <Hans-Joachim.Multhaupt@git.noratargo.net>
2011-06-19 12:56:39 +0200

    Removed a deprecated class and added some documentation.
    Actually, I wanted to test out the documentation-features for the
    README.* file.

--- src/net/noratargo/siJACK/instanciators/ClassInstantiator.java
+++ src/net/noratargo/siJACK/instanciators/ClassInstantiator.java
@@ -2,6 +2,13 @@ package net.noratargo.siJACK.instanciators;
 
 import net.noratargo.siJACK.interfaces.ParameterInstanciator;
 
+/**
+ * This instantiator can create {@link Class} instances for a fully qualified class - as far as it is accessible
+ * 
+ * @author HMulthaupt
+ */
+/* Supressing the warnings was necessary, since I was not allowed to cast the Class.class Element to Class<Class<?>> */
+@SuppressWarnings("rawtypes")
 public class ClassInstantiator implements ParameterInstanciator<Class> {
 
 	@Override
